dcbb013c15c2de8d9f6d0e23c573b776c72b3063
Ridhanovita <novitaridha796@gmail.com>
2025-04-17 11:31:37 +0700

    menambahkan +1 pda mid step 4

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -5,84 +5,101 @@ int arr[20], B[20];
 
 int n;
 
-void input() {
-    while(true) {
+void input()
+{
+    while (true)
+    {
         cout << "Masukkan Panjang element arry: ";
         cin >> n;
 
-        if (n <= 20) {
+        if (n <= 20)
+        {
             break;
         }
-        else {
+        else
+        {
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
 
     cout << "\n==========================";
     cout << "\nInputkan Isi Element Array";
-    cout << "\n==========================";
+    cout << "\n==========================" << endl;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << "Array index ke - " << i << ": ";
         cin >> arr[i];
     }
 }
 
-void mergeSort(int low, int high) {
-    if (low >= high) {  //step 1
-        return;         // step 1.a
+void mergeSort(int low, int high)
+{
+    if (low >= high)
+    {           // step 1
+        return; // step 1.a
     }
 
-    int mid = (low + high)/2; //step 2
+    int mid = (low + high) / 2; // step 2
 
-    //step 3
-    //fungsi rekursi - memanggil diri sendiri
-    mergeSort(low, mid);      //step 3.a
-    mergeSort(mid + 1, high); //step 3.b
+    // step 3
+    // fungsi rekursi - memanggil diri sendiri
+    mergeSort(low, mid);      // step 3.a
+    mergeSort(mid + 1, high); // step 3.b
 
-     //step 4
-     int i = low;    //step 4.a
-     int j = mid;    //step 4.b
-     int k = low;    //step 4.c
- 
-     while (i <= mid && j <= high) { //step 4.d
-         if (arr[i] <= arr[j]) {     //step 4.d.i
-             B[k] = arr[i];
-             i++;
-         } else {                    //step 4.d.ii
-             B[k] = arr[j];
-             j++;
-         }
-         k++;
-     }
- 
-     while (j <= high) { //step 4.e
-         B[k] = arr[j];  //step 4.e.i
-         j++;            //step 4.e.ii
-         k++;            //step 4.e.iii
-     }
- 
-     while (i <= mid) {  //step 4.f
-         B[k] = arr[i];  //step 4.f.i
-         i++;            //step 4.f.ii
-         k++;            //step 4.f.iii
-     }
+    // step 4
+    int i = low;     // step 4.a
+    int j = mid + 1; // step 4.b
+    int k = low;     // step 4.c
 
-     //step 5
-     for (int x = low; x <= high; x++) {
+    while (i <= mid && j <= high)
+    { // step 4.d
+        if (arr[i] <= arr[j])
+        { // step 4.d.i
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        { // step 4.d.ii
+            B[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (j <= high)
+    {                  // step 4.e
+        B[k] = arr[j]; // step 4.e.i
+        j++;           // step 4.e.ii
+        k++;           // step 4.e.iii
+    }
+
+    while (i <= mid)
+    {                  // step 4.f
+        B[k] = arr[i]; // step 4.f.i
+        i++;           // step 4.f.ii
+        k++;           // step 4.f.iii
+    }
+
+    // step 5
+    for (int x = low; x <= high; x++)
+    {
         arr[x] = B[x];
-     }
+    }
 }
 
-void output() {
-    cout << "\nData setelah diurutkn (MergeSort): ";
-    for (int i = 0; i < n; i++) {
+void output()
+{
+    cout << "\nData setelah diurutkan (MergeSort): ";
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-int main() {
+int main()
+{
     input();
     mergeSort(0, n - 1);
     output();
